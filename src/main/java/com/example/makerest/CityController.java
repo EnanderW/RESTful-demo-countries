cf8cdf1f88f79187067af2800e408027f7f1436e
BuildTools <unconfigured@null.spigotmc.org>
2021-10-20 15:50:43 +0200

    Initial Commit

--- src/main/java/com/example/makerest/CityController.java
+++ src/main/java/com/example/makerest/CityController.java
@@ -5,58 +5,90 @@ import lombok.Setter;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 public class CityController {
 
-    private CityService service = CityService.INSTANCE;
+    private Map<String, City> cities = new HashMap<>();
+
+    private City selected;
 
     @PutMapping("/city/create")
     public CityPayload createCity(@RequestBody City city) {
-        city = service.saveCity(city);
+        save(city);
+
         return new CityPayload(city.getName(), city.getPopulation(), city.getSize());
     }
 
     @GetMapping("/city/all")
     public List<CityPayload> getAllCities() {
-        return service.getCities()
+        return cities.values()
                 .stream()
                 .map(city -> new CityPayload(city.getName(), city.getPopulation(), city.getSize()))
                 .collect(Collectors.toList());
     }
 
-    @DeleteMapping("/city/delete/{cityName}")
-    public String deleteCity(@PathVariable("cityName") String cityName, HttpServletResponse response) {
-        if (!service.deleteCity(cityName)) {
+    @PostMapping("/city/select/{cityName}")
+    public String selectCity(@PathVariable("cityName") String cityName, HttpServletResponse response) {
+        selected = getCity(cityName);
+        if (selected == null) {
             response.setStatus(404);
             return "There is no city named '" + cityName + "'";
         }
 
-        return "You have deleted '" + cityName + "'";
+        return "Selected city '" + cityName + "'";
     }
 
-    @GetMapping("/city/info/{cityName}")
-    public CityPayload getInfo(@PathVariable("cityName") String cityName, HttpServletResponse response) {
-        City city = service.getCity(cityName);
-        if (city == null) {
+    @DeleteMapping("/city/delete")
+    public String deleteCity(HttpServletResponse response) {
+        if (selected == null) {
+            response.setStatus(404);
+            return "You have not selected a city.";
+        }
+
+        remove(selected);
+        String name = selected.getName();
+        selected = null;
+        return "You have deleted '" + name + "'";
+    }
+
+    @GetMapping("/city/info")
+    public CityPayload getInfo(HttpServletResponse response) {
+        if (selected == null) {
             response.setStatus(404);
             return null;
         }
 
-        return new CityPayload(city.getName(), city.getPopulation(), city.getSize());
+        return new CityPayload(selected.getName(), selected.getPopulation(), selected.getSize());
     }
 
-    @PutMapping("/city/update/{cityName}")
-    public CityPayload updateCity(@PathVariable("cityName") String cityName, @RequestBody CityUpdate cityUpdate, HttpServletResponse response) {
-        if (!service.updateCity(cityName, cityUpdate)) {
+    @PutMapping("/city/update")
+    public CityPayload updateCity(@RequestBody CityUpdate cityUpdate, HttpServletResponse response) {
+        if (selected == null) {
             response.setStatus(404);
             return null;
         }
 
-        City city = service.getCity(cityName);
-        return new CityPayload(city.getName(), city.getPopulation(), city.getSize());
+        selected.setPopulation(cityUpdate.getPopulation());
+        selected.setSize(cityUpdate.getSize());
+        return new CityPayload(selected.getName(), selected.getPopulation(), selected.getSize());
+    }
+
+    public void save(City city) {
+        cities.put(city.getName().toLowerCase(), city);
+    }
+
+    public City getCity(String name) {
+        return cities.get(name.toLowerCase());
+    }
+
+    public void remove(City city) {
+        cities.remove(city.getName().toLowerCase());
     }
 
     @Getter
--- src/main/java/com/example/makerest/CountryController.java
+++ src/main/java/com/example/makerest/CountryController.java
@@ -1,63 +1,14 @@
 package com.example.makerest;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class CountryController {
 
-    private CountryService service = new CountryService();
-
     @PutMapping("/country/create")
-    public CountryPayload createCountry(@RequestBody CountryCreate countryCreate, HttpServletResponse response) {
-        Country country = service.createCountry(countryCreate);
-        if (country == null) {
-            response.setStatus(404);
-            return null;
-        }
+    public void createCountry() {
 
-        return new CountryPayload(country);
-    }
-
-    @GetMapping("/country/all")
-    public List<CountryPayload> getAllCountries() {
-        return service.getCountries()
-                .stream()
-                .map(CountryPayload::new)
-                .collect(Collectors.toList());
-    }
-
-    @Getter
-    @Setter
-    public static final class CountryPayload {
-        private String name;
-        private String region;
-        private String language;
-        private String capital;
-
-        public CountryPayload(Country country) {
-            this.name = country.getName();
-            this.region = country.getRegion();
-            this.language = country.getLanguage();
-            this.capital = "/city/info/" + country.getCapital().getName();
-        }
-    }
-
-    @Getter
-    @Setter
-    public static final class CountryCreate {
-        private String name;
-        private String region;
-        private String language;
-        private String capital;
     }
 
 }
